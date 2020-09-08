4167b9e0faa93a2e215a425b207f8a6f43180b88
Nick <njhuckov@gmail.com>
2020-09-08 10:23:07 +0300

    Create squareseqdigit.py

---
+++ squareseqdigit.py
@@ -0,0 +1,17 @@
+
+def squareSequenceDigit(n):
+    
+    result = 0
+    return result
+    
+    
+    
+    
+if __name__ == "__main__":
+    squareSequenceDigit(1)
+    squareSequenceDigit(2)
+    squareSequenceDigit(7)
+    squareSequenceDigit(12)
+    squareSequenceDigit(17)
+    squareSequenceDigit(27)
+    
